ce83a0185c7d28ce62feea95340922843a536042
NickCodes20 <nickcodes20@gmail.com>
2023-03-20 06:55:12 -0500

    Added screen size dropdown (not functional yet), and cleaned up GUI

--- bhubfuncs.py
+++ bhubfuncs.py
@@ -2,7 +2,19 @@ import subprocess
 import os
 
 
+# TODO ask user if they're working out of a folder structure, if so change localhost to folder_structured_localhost var
+# TODO ^^^ accomplish this by using a checkbox
 class BhubFunctionality:
+
+    def __init__(self):
+        self.localhost = "http://127.0.0.1:5500/index.html"
+        self.folder_structured_localhost = "http://127.0.0.1:5500/html/index.html"
+        self.processes = []
+        self.browser_dict = {}
+        self.desktop = "fullscreen"
+        self.tablet = "800x600"
+        self.mobile = "400x800"
+        self.size_menu = {"Desktop": self.desktop, "Tablet": self.tablet, "Mobile": self.mobile}
 
     home_dir = os.path.expanduser("~")
     if os.name == "nt":  # Windows
@@ -11,76 +23,74 @@ class BhubFunctionality:
         edge = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
         brave = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
         opera = os.path.join(home_dir, "AppData", "Local", "Programs", "Opera", "Launcher.exe")
+        # safari = os.path.join(home_dir, "Applications", "Safari.app")
     elif os.name == "posix":  # Mac or Linux
         chrome = os.path.join(home_dir, "Applications", "Google Chrome.app")
         firefox = os.path.join(home_dir, "Applications", "Firefox.app")
         edge = os.path.join(home_dir, "Applications", "Microsoft Edge.app")
         brave = os.path.join(home_dir, "Applications", "Brave Browser.app")
         opera = os.path.join(home_dir, "Applications", "Opera.app")
-        # safari = os.path.join(home_dir, "Applications", "Safari.app")
+        safari = os.path.join(home_dir, "Applications", "Safari.app")
 
-    localhost = "http://127.0.0.1:5500/index.html"
-    folder_structured_localhost = "http://127.0.0.1:5500/html/index.html"
-
-    processes = []
-
-    browser_list = []
-
-    # TODO tweak launch() code to check if browser is installed, if so then launch it
+    # TODO if browser is not installed ask user if they want to install it, launch default browser with link to install
     def launch(self):
         """Launches browsers from the browser list."""
-        if self.browser_list:
-            for browser in self.browser_list:
-                process = subprocess.Popen(browser)
-                self.processes.append(process)
-            print("Launch Successful")
+        if self.browser_dict:
+            for browser, path in self.browser_dict.items():
+                if os.path.exists(path[0]):
+                    process = subprocess.Popen(path)
+                    self.processes.append(process)
+                    print(f"Launch Successful: {browser}")
+                else:
+                    print(f"{browser} is not installed")
         else:
             print("Please select a browser.")
 
+    # TODO create functions to add screen sizes to OptionMenu, options should be named "Desktop", "Tablet", and "Mobile"
     def append_chrome(self, box_value):
         """Appends Chrome browser to browser list """
         if box_value is True:
-            self.browser_list.append([self.chrome, self.localhost])
+            self.browser_dict["Chrome"] = [self.chrome, self.localhost]
         elif box_value is False:
-            self.browser_list.remove([self.chrome, self.localhost])
-        print(self.browser_list)
+            del self.browser_dict["Chrome"]
+        print(self.browser_dict)
 
     def append_firefox(self, box_value):
         """Appends Firefox browser to browser list """
         if box_value is True:
-            self.browser_list.append([self.firefox, self.localhost])
+            self.browser_dict["Firefox"] = [self.firefox, self.localhost]
         elif box_value is False:
-            self.browser_list.remove([self.firefox, self.localhost])
-        print(self.browser_list)
+            del self.browser_dict["Firefox"]
+        print(self.browser_dict)
 
     def append_edge(self, box_value):
         """Appends Edge browser to browser list """
         if box_value is True:
-            self.browser_list.append([self.edge, self.localhost])
+            self.browser_dict["Edge"] = [self.edge, self.localhost]
         elif box_value is False:
-            self.browser_list.remove([self.edge, self.localhost])
-        print(self.browser_list)
+            del self.browser_dict["Edge"]
+        print(self.browser_dict)
 
     def append_brave(self, box_value):
         """Appends Brave browser to browser list """
         if box_value is True:
-            self.browser_list.append([self.brave, self.localhost])
+            self.browser_dict["Brave"] = [self.brave, self.localhost]
         elif box_value is False:
-            self.browser_list.remove([self.brave, self.localhost])
-        print(self.browser_list)
+            del self.browser_dict["Brave"]
+        print(self.browser_dict)
 
     def append_opera(self, box_value):
         """Appends Opera browser to browser list """
         if box_value is True:
-            self.browser_list.append([self.opera, self.localhost])
+            self.browser_dict["Opera"] = [self.opera, self.localhost]
         elif box_value is False:
-            self.browser_list.remove([self.opera, self.localhost])
-        print(self.browser_list)
+            del self.browser_dict["Opera"]
+        print(self.browser_dict)
 
     def append_safari(self, box_value):
         """Appends Safari browser to browser list """
         if box_value is True:
-            self.browser_list.append([self.safari, self.localhost])
+            self.browser_dict["Safari"] = [self.safari, self.localhost]
         elif box_value is False:
-            self.browser_list.remove([self.safari, self.localhost])
-        print(self.browser_list)
+            del self.browser_dict["Safari"]
+        print(self.browser_dict)
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import customtkinter
+import os
 import bhubfuncs
 
 bhf = bhubfuncs.BhubFunctionality()
 
-# TODO add safari
-# TODO tweak launch function to check if checked browsers are installed before launching
-# TODO if browser is not installed ask user if they want to install it, and launch default browser with link to install
 # TODO add option to choose size of window upon launch for each browser, Full Screen (default), iPad, or Mobile
-# TODO add functionality to be compatible across operating systems
+# TODO ^^^ this could be done by changing the checkboxes to dropdown arrows, and having checkbox options for sizes
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
+# TKinter arguments
+PRIMARY = "#2e3034"
+ACCENT = "#4f535a"
+HOVER_COLOR = "#1e2c40"
+
 root = customtkinter.CTk()
-root.geometry("500x500")
+root.title("BrowserHub")
+root.iconbitmap("Internet-Explorer.ico")
+
+if os.name == "nt":  # Windows OS window size
+    root.geometry("400x450")
+elif os.name == "posix":  # macOS window size
+    root.geometry("500x500")
 
 check_chrome = customtkinter.BooleanVar()
 check_firefox = customtkinter.BooleanVar()
@@ -41,54 +50,80 @@ def select_all(checkboxes):
 
 
 frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
+frame.grid(pady=20, padx=60, rowspan=1, columnspan=1)
 
 label = customtkinter.CTkLabel(master=frame, text="Select Browsers", font=("Sans Serif", 24))
-label.pack(pady=12, padx=10)
+label.grid(pady=12, padx=10, row=0, column=0, columnspan=2, sticky="nsew")
 
 # Chrome's checkbox
 chromebox = customtkinter.CTkCheckBox(master=frame, text="Chrome", command=bhf.append_chrome, variable=check_chrome)
 chromebox.configure(command=lambda: bhf.append_chrome(check_chrome.get()))
-chromebox.pack(pady=12, padx=10)
+chromebox.grid(pady=12, padx=10, row=2, column=0)
+# Chrome's drop down menu
+chromedrop = customtkinter.CTkOptionMenu(master=frame, values=list(bhf.size_menu.keys()), fg_color=PRIMARY,
+                                         button_color=ACCENT, dropdown_fg_color=PRIMARY,
+                                         dropdown_hover_color=HOVER_COLOR)
+chromedrop.grid(pady=12, padx=10, row=2, column=1)
 
 # Firefox's checkbox
 foxbox = customtkinter.CTkCheckBox(master=frame, text="Firefox", command=bhf.append_firefox, variable=check_firefox)
 foxbox.configure(command=lambda: bhf.append_firefox(check_firefox.get()))
-foxbox.pack(pady=12, padx=10)
+foxbox.grid(pady=12, padx=10)
+# Firefox's drop down menu
+foxdrop = customtkinter.CTkOptionMenu(master=frame, values=list(bhf.size_menu.keys()), fg_color=PRIMARY,
+                                      button_color=ACCENT, dropdown_hover_color=HOVER_COLOR)
+foxdrop.grid(pady=12, padx=10, row=3, column=1)
 
 # Edge's checkbox
 edgebox = customtkinter.CTkCheckBox(master=frame, text="Edge", command=bhf.append_edge, variable=check_edge)
 edgebox.configure(command=lambda: bhf.append_edge(check_edge.get()))
-edgebox.pack(pady=12, padx=10)
+edgebox.grid(pady=12, padx=10)
+# Edge's drop down menu
+edgedrop = customtkinter.CTkOptionMenu(master=frame, values=list(bhf.size_menu.keys()), fg_color=PRIMARY,
+                                       button_color=ACCENT, dropdown_hover_color=HOVER_COLOR)
+edgedrop.grid(pady=12, padx=10, row=4, column=1)
 
 # Brave's checkbox
 bravebox = customtkinter.CTkCheckBox(master=frame, text="Brave", command=bhf.append_brave, variable=check_brave)
 bravebox.configure(command=lambda: bhf.append_brave(check_brave.get()))
-bravebox.pack(pady=12, padx=10)
+bravebox.grid(pady=12, padx=10)
+# Brave's drop down menu
+bravedrop = customtkinter.CTkOptionMenu(master=frame, values=list(bhf.size_menu.keys()), fg_color=PRIMARY,
+                                        button_color=ACCENT, dropdown_hover_color=HOVER_COLOR)
+bravedrop.grid(pady=12, padx=10, row=5, column=1)
 
 # Opera's checkbox
 operabox = customtkinter.CTkCheckBox(master=frame, text="Opera", command=bhf.append_opera, variable=check_opera)
 operabox.configure(command=lambda: bhf.append_opera(check_opera.get()))
-operabox.pack(pady=12, padx=10)
+operabox.grid(pady=12, padx=10)
+# Opera's drop down menu
+operadrop = customtkinter.CTkOptionMenu(master=frame, values=list(bhf.size_menu.keys()), fg_color=PRIMARY,
+                                        button_color=ACCENT, dropdown_hover_color=HOVER_COLOR)
+operadrop.grid(pady=12, padx=10, row=6, column=1)
 
 # Safari's checkbox
-# safaribox = customtkinter.CTkCheckBox(master=frame, text="Safari", command=bhf.append_safari, variable=check_safari)
-# safaribox.configure(command=lambda: bhf.append_safari(check_safari.get()))
-# safaribox.pack(pady=12, padx=10)
+if os.name == "posix":  # Only adds safari checkbox to macOS
+    safaribox = customtkinter.CTkCheckBox(master=frame, text="Safari", command=bhf.append_safari,
+                                          variable=check_safari)
+    safaribox.configure(command=lambda: bhf.append_safari(check_safari.get()))
+    safaribox.grid(pady=12, padx=10)
+    # Safari's drop down menu
+    safaridrop = customtkinter.CTkOptionMenu(master=frame, values=list(bhf.size_menu.keys()), fg_color=PRIMARY,
+                                             button_color=ACCENT, dropdown_hover_color=HOVER_COLOR)
+    safaridrop.grid(pady=12, padx=10, row=7, column=1)
+
 
 # Checkbox List
-all_checkboxes = [chromebox, foxbox, edgebox, bravebox, operabox] # Add safaribox
+all_checkboxes = [chromebox, foxbox, edgebox, bravebox, operabox]  # Add safaribox
 
 select_all_switch = customtkinter.CTkSwitch(master=frame, switch_width=40, switch_height=20, border_width=2,
                                             corner_radius=10, progress_color="green", text="Select All",
                                             command=select_all, variable=select_all_var)
-select_all_switch.pack(pady=12, padx=10)
 select_all_switch.configure(command=lambda: select_all(all_checkboxes))
-
-select_all_switch.pack()
+select_all_switch.grid(pady=12, padx=10)
 
 # Launch button launches all browsers that have been selected via checkbox
 button = customtkinter.CTkButton(master=frame, text="Launch", command=bhf.launch)
-button.pack(pady=12, padx=10)
+button.grid(pady=12, padx=10, columnspan=2, sticky="nsew")
 
 root.mainloop()
